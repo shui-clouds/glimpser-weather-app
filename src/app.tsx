55843bc938bdd100029729208ab671d452249aac
shui-clouds <57289318+shui-clouds@users.noreply.github.com>
2024-10-08 12:41:10 +0100

    set up page layout

---
+++ src/app.tsx
@@ -0,0 +1,81 @@
+import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import { useState } from 'react'
+
+const people = [
+  { id: 1, name: 'Leslie Alexander' },
+]
+
+export default function App() {
+  const [query, setQuery] = useState('')
+  const [selectedPerson, setSelectedPerson] = useState(null)
+
+  const filteredPeople =
+    query === ''
+      ? people
+      : people.filter((person) => {
+        return person.name.toLowerCase().includes(query.toLowerCase())
+      })
+
+  return (
+    <main className="flex flex-col justify-center items-center px-12 py-12">
+      <h1 className="text-center text-3xl font-semibold">Weather App</h1>
+      <Combobox
+        className="w-full max-w-64"
+        as="div"
+        value={selectedPerson}
+        onChange={(person) => {
+          setQuery('')
+          setSelectedPerson(person)
+        }}
+      >
+        <div className="relative mt-8">
+          <ComboboxInput
+            className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            onChange={(event) => setQuery(event.target.value)}
+            onBlur={() => setQuery('')}
+            displayValue={(person) => person?.name}
+            placeholder='Enter a location...'
+          />
+          <ComboboxButton className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+            <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </ComboboxButton>
+
+          {filteredPeople.length > 0 && (
+            <ComboboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {filteredPeople.map((person) => (
+                <ComboboxOption
+                  key={person.id}
+                  value={person}
+                  className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+                >
+                  <span className="block truncate group-data-[selected]:font-semibold">{person.name}</span>
+
+                  <span className="absolute inset-y-0 right-0 hidden items-center pr-4 text-indigo-600 group-data-[selected]:flex group-data-[focus]:text-white">
+                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                  </span>
+                </ComboboxOption>
+              ))}
+            </ComboboxOptions>
+          )}
+        </div>
+      </Combobox>
+
+      <div className='md:flex gap-x-6 mt-2'>
+        <p>hello</p>
+        <p>hello</p>
+        <p>hello</p>
+        <p>hello</p>
+        <p>hello</p>
+        <p>hello</p>
+        <p>hello</p>
+        <p>hello</p>
+
+
+      </div>
+
+
+
+    </main>
+  )
+}
